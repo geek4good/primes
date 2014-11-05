15e9abca348ad872febdc09f67d48e7a954f47e2
Lucas Mbiwe <lucas@geek4good.com>
2014-11-06 00:00:22 +0100

    No longer expose the enumerator
    * make the prime number enumerator private
    * add #first for getting the first n primes as an array
    * add #nth for getting the nth prime number

--- lib/primes/sieve_of_eratosthenes.rb
+++ lib/primes/sieve_of_eratosthenes.rb
@@ -1,5 +1,18 @@
 module Primes
   class SieveOfEratosthenes
+    def first(n)
+      index = Integer(n)
+      return [] if index < 1
+
+      primes.first(n)
+    end
+
+    def nth(n)
+      first(n).last
+    end
+
+    private
+
     def primes
       prime_numbers = []
       (2..Float::INFINITY).lazy.select { |number|
--- test/lib/primes/sieve_of_eratosthenes_test.rb
+++ test/lib/primes/sieve_of_eratosthenes_test.rb
@@ -4,9 +4,15 @@ module Primes
   describe SieveOfEratosthenes do
     subject { SieveOfEratosthenes.new }
 
-    describe "#primes" do
-      it "returns an ordered sequence of the prime numbers" do
-        assert_equal(subject.primes.first(12), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
+    describe "#first" do
+      it "returns the first n prime numbers" do
+        assert_equal(subject.first(12), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
+      end
+    end
+
+    describe "#nth" do
+      it "returns the nth prime number" do
+        assert_equal(17, subject.nth(7))
       end
     end
   end
