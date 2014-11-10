bbb8cb98820db5134758b41da937cf83e580436d
Lucas Mbiwe <lucas@geek4good.com>
2014-11-10 02:57:34 +0100

    Add a simple CLI
    * add bin/primes
    * provide parameter 'size' to set the multiplication table's size
    * set default size to 10

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,3 +1,10 @@
 require "primes/version"
 require "primes/sieve"
 require "primes/multiplication_table"
+
+module Primes
+  def self.multiplication_table(options)
+    size = options[:size].to_i
+    MultiplicationTable.new(Sieve.new.first(size))
+  end
+end
